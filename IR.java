4e01d053ac9ee02889d3d216d815f0fce741ffde
gal <gk29@rice.edu>
2023-09-08 15:27:04 -0500

    started IR

---
+++ IR.java
@@ -0,0 +1,12 @@
+/*
+ * This class is an internal representation for one parser. 
+ * it points to the head and the tail of a doubly linked list that each node in the list is
+ * a lineIR, that is a line internal representation of a single line 
+ */
+
+public class IR {
+    
+    // fields of one internal representation:
+    lineIR head;
+    lineIR tail;
+}
